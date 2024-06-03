8dd361fffaad59d092549a873c397d20f1c59d0f
jmisshela <138009686+jmisshela@users.noreply.github.com>
2024-06-03 19:04:14 +0800

    Removed unnecessary files & added footer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Velkommen til reactværappen!</h1>
+      <footer>
+        This project was coded by Jane Misshela, it is open-sourced on GitHub
+        and hosted on Netlify
+      </footer>
     </div>
   );
 }
-
-export default App;
