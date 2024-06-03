ecf7cef63dc2fab240d9d610d2f9459a65129e7a
jmisshela <138009686+jmisshela@users.noreply.github.com>
2024-06-03 20:28:47 +0800

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,34 +3,36 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Velkommen til reactværappen!</h1>
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://www.linkedin.com/in/jmisshela"
-          target="_blank"
-          rel="noreferrer"
-        >
-          {" "}
-          Jane Misshela
-        </a>
-        , it is open-sourced on{" "}
-        <a
-          href="https://github.com/jmisshela/react-vaer-appen"
-          target="_blank"
-          rel="noreferrer"
-        >
-          GitHub
-        </a>{" "}
-        and hosted on{" "}
-        <a
-          href="https://rad-salamander-c82269.netlify.app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Netlify
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Velkommen til reactværappen!</h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/jmisshela"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            Jane Misshela
+          </a>
+          , it is open-sourced on{" "}
+          <a
+            href="https://github.com/jmisshela/react-vaer-appen"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>{" "}
+          and hosted on{" "}
+          <a
+            href="https://rad-salamander-c82269.netlify.app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
