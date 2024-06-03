70019fa85d193566ba4600a99ef617fd94c98d03
jmisshela <138009686+jmisshela@users.noreply.github.com>
2024-06-03 20:07:41 +0800

    Added links to the footer

--- src/App.js
+++ src/App.js
@@ -5,8 +5,31 @@ export default function App() {
     <div className="App">
       <h1>Velkommen til reactværappen!</h1>
       <footer>
-        This project was coded by Jane Misshela, it is open-sourced on GitHub
-        and hosted on Netlify
+        This project was coded by{" "}
+        <a
+          href="https://www.linkedin.com/in/jmisshela"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {" "}
+          Jane Misshela
+        </a>
+        , it is open-sourced on{" "}
+        <a
+          href="https://github.com/jmisshela/react-vaer-appen"
+          target="_blank"
+          rel="noreferrer"
+        >
+          GitHub
+        </a>{" "}
+        and hosted on{" "}
+        <a
+          href="https://rad-salamander-c82269.netlify.app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Netlify
+        </a>
       </footer>
     </div>
   );
