eb4c1a8650cd078204a4eaa8c56ea6efb476e21d
jmisshela <138009686+jmisshela@users.noreply.github.com>
2024-06-04 14:32:54 +0800

    Added Weather component, improved the UI, and updated Babel dependencies
    Resolved Babel warning: Add @babel/plugin-transform-private-property-in-object
    
    This commit addresses a deprecation warning related to the `@babel/plugin-proposal-private-property-in-object` package.
    
    I've added `@babel/plugin-transform-private-property-in-object` as the recommended alternative to ensure compatibility with future versions of Babel and JavaScript.

--- src/Weather.js
+++ src/Weather.js
@@ -5,17 +5,43 @@ export default function Weather() {
   return (
     <div className="Weather">
       <form>
-        <input type="search" placeholder="Enter a city..." />
-        <input type="submit" value="Search" />
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city..."
+              className="form-control"
+            />
+          </div>
+          <div className="col-3">
+            <input type="submit" value="Search" className="btn btn-primary" />
+          </div>
+        </div>
       </form>
-      <h1>Oslo</h1>
-      {/*
-      <p>
-        Tuesday 9:15, broken clouds
-        <br />
-        Humidity: 60%, Wind: 2.03km/h
-      </p>
-  */}
+      <div className="row">
+        <div className="col-7">
+          <h1>Oslo</h1>
+          <ul>
+            <li>Tuesday 9:15</li>
+            <li>Broken Clouds</li>
+          </ul>
+          <div className="temperature-container">
+            <div className="temperature">23</div>
+            <div className="units">°C | °F</div>
+          </div>
+        </div>
+        <div className="col-5 iconBlock">
+          <img
+            src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/broken-clouds-night.png"
+            alt="broken-clouds"
+          />
+          <br />
+          <ul>
+            <li>Humidity: 4km/h</li>
+            <li>Wind: 4 km/h</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
